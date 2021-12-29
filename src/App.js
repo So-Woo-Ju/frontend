213443365dccea1528dcc6c2974f580858d8c712
juhyojeong <juhyojeong58@gmail.com>
2021-12-29 13:03:42 +0900

    feat: redirection 추가
    페이지 접근 제한
    
    
    Former-commit-id: 2de7d368d52b1a7f8ef6dcf9ac7e18ffcbd57433

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import MainPage from "./pages/MainPage";
 import Loading from "./pages/Loading";
@@ -7,19 +7,64 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Mypage from "./pages/Mypage";
 import ErrorPage from "./pages/Errorpage";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { user } = useSelector(({ user }) => ({
+    user: user.user,
+  }));
   return (
     <div style={{ height: window.innerHeight }}>
       <Header />
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/loading" element={<Loading />} />
-        <Route path="/result" element={<ResultPage />} />
-        <Route path="/mypage" element={<Mypage />} />
-        <Route path="/error" element={<ErrorPage />} />
+        <Route
+          path="/"
+          element={
+            user === null ? <Navigate replace to="/login" /> : <MainPage />
+          }
+        />
+        <Route
+          path="/login"
+          element={user === null ? <Login /> : <Navigate replace to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={user === null ? <Signup /> : <Navigate replace to="/" />}
+        />
+        <Route
+          path="/loading"
+          element={
+            user === null ? <Navigate replace to="/login" /> : <Loading />
+          }
+        />
+        <Route
+          path="/result"
+          element={
+            user === null ? <Navigate replace to="/login" /> : <ResultPage />
+          }
+        />
+        <Route
+          path="/mypage"
+          element={
+            user === null ? <Navigate replace to="/login" /> : <Mypage />
+          }
+        />
+        <Route
+          path="/error"
+          element={
+            user === null ? <Navigate replace to="/login" /> : <ErrorPage />
+          }
+        />
+        <Route
+          path="*"
+          element={
+            user === null ? (
+              <Navigate replace to="/login" />
+            ) : (
+              <Navigate replace to="/" />
+            )
+          }
+        />
       </Routes>
     </div>
   );
