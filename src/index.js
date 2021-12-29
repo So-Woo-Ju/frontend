00fc4841e4d9ed89593ed98eef1dc5d1fe847d8e
juhyojeong <juhyojeong58@gmail.com>
2021-12-29 11:09:46 +0900

    feat: redux 추가
    Former-commit-id: e6a2e57da78f792da1d06737ceec3dbb85362094

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "antd/dist/antd.css";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root"),
 );
