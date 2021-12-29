83b20435bb53122242687b8ba1f9007053be4483
juhyojeong <juhyojeong58@gmail.com>
2021-12-29 10:23:16 +0900

    feat: 헤더 추가
    메인페이지, 마이페이지, 로그아웃/로그인 탭으로 이동 가능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <div style={{ height: window.innerHeight }}>
+      <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
       </Routes>
---
+++ src/components/Header.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import { Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+
+const Container = styled.div`
+  height: 50px;
+  border-bottom: 1px solid lightgrey;
+`;
+const LeftBox = styled(Link)`
+  color: black;
+  margin: 5px;
+  cursor: pointer;
+  :hover {
+    color: gray;
+  }
+`;
+const RightBox = styled.div`
+  display: flex;
+  float: right;
+  margin: 7px 10px -7px 0;
+  cursor: pointer;
+`;
+const UserBox = styled(Link)`
+  color: black;
+  display: flex;
+  :hover {
+    color: gray;
+  }
+`;
+const StyledUsername = styled.p`
+  font-size: 15px;
+  padding-left: 7px;
+  padding-top: 5px;
+`;
+const StyledLink = styled(Link)`
+  font-size: 13px;
+  padding: 7px 7px 0 15px;
+`;
+
+function Header() {
+  return (
+    <Container>
+      <LeftBox to="/">
+        <AiFillHome size={40} />
+      </LeftBox>
+      <RightBox>
+        <UserBox to="/mypage">
+          <Avatar icon={<UserOutlined />} />
+          <StyledUsername>username</StyledUsername>
+        </UserBox>
+        <StyledLink to="/login">logout</StyledLink>
+      </RightBox>
+    </Container>
+  );
+}
+
+export default Header;
