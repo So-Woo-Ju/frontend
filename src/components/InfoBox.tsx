d0bdde1917d314bcd2972014ac73a8fd24cd6725
juhyojeong <juhyojeong58@gmail.com>
2022-02-17 13:38:04 +0900

    feat: 서비스 처리 과정 추가
    About 창에 서비스 처리 과정 추가

---
+++ src/components/InfoBox.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const Content = styled.div`
+  margin: 50px 30px;
+`;
+
+interface BoxType {
+  imgType: string;
+}
+
+const InfoBox: React.FC<BoxType> = ({ imgType, children }) => {
+  const [imgSrc, setImgSrc] = useState("");
+
+  useEffect(() => {
+    setImgSrc("/icon/" + imgType + ".png");
+  }, []);
+
+  return (
+    <Container>
+      {imgType === "ai" ? (
+        <>
+          <Content>{children}</Content>
+          <img src={imgSrc} width={300} />
+        </>
+      ) : (
+        <>
+          <img src={imgSrc} width={300} />
+          <Content>{children}</Content>
+        </>
+      )}
+    </Container>
+  );
+};
+
+export default InfoBox;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import InfoBox from "../components/InfoBox";
+import styled from "styled-components";
+
+const ProcTitle = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+`;
+const ProcContent = styled.p`
+  width: 400px;
+  font-size: 16px;
+`;
+const ProcRef = styled.p`
+  font-size: 13px;
+  color: gray;
+`;
 
 const About = () => {
-  return <div>about</div>;
+  return (
+    <div>
+      <InfoBox imgType="video">
+        <ProcTitle>영상 입력</ProcTitle>
+        <ProcContent>유튜브 링크 및 로컬 파일을 입력합니다</ProcContent>
+      </InfoBox>
+      <InfoBox imgType="ai">
+        <ProcTitle>인공지능 처리</ProcTitle>
+        <ProcContent>
+          약 0분 간 인공지능을 사용해 입력된 영상에 따른 폐쇄형 자막이
+          생성됩니다
+        </ProcContent>
+      </InfoBox>
+      <InfoBox imgType="cc">
+        <ProcTitle>자막 및 스크립트 출력</ProcTitle>
+        <ProcContent>
+          생성된 폐쇄형 자막 및 스크립트가 화면에 출력됩니다
+        </ProcContent>
+        <ProcRef>
+          * 유튜브 링크가 주어진 경우 영상 내 자막이 제공되지 않습니다
+        </ProcRef>
+      </InfoBox>
+    </div>
+  );
 };
 
 export default About;
