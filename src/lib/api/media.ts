7f764e5c61435bff2334d75839873e866df1bebd
juhyojeong <juhyojeong58@gmail.com>
2022-02-22 20:35:07 +0900

    chore: token expire date 추가

--- src/lib/api/media.ts
+++ src/lib/api/media.ts
@@ -71,23 +71,23 @@ export const load = (email: string) => {
 };
 
 export const upload = async (type: Number, file: File | null, url: string) => {
-  /*if (type === 1) {
-    const { preSignedUrl } = await client({
-      url: "/media/get-presigned-url",
-      method: "post",
+  if (type === 1) {
+    const res = await client({
+      url: "/media/video/presigned-url",
+      method: "get",
     });
+    /*
     const res = await fetch(preSignedUrl, {
       method: "PUT",
       body: file,
-    });
+    });*/
     console.log(res);
   } else {
-    const res = await client({
+    /*const res = await client({
       url: "/media/upload",
       method: "post",
       data: { url },
-    });
-  }*/
-
+    });*/
+  }
   return { data: "" };
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -62,7 +62,9 @@ const Login: React.FunctionComponent<LoginType> = ({ setIsLogin }) => {
     const refresh_token = data.refreshToken;
     const access_token = data.accessToken;
     const token_exp = data.tokenExp;
-    cookies.set("token_exp", token_exp);
+    cookies.set("token_exp", token_exp, {
+      expires: new Date(token_exp),
+    });
     cookies.set("access_token", access_token, {
       expires: new Date(Date.now() + 1000 * 60 * 15),
     });
