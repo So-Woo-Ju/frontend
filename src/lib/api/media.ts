c4a7131640a87df65daf97d009879c5ca2009a7a
juhyojeong <juhyojeong58@gmail.com>
2022-05-27 15:12:21 +0900

    feat: script 출력

--- src/lib/api/media.ts
+++ src/lib/api/media.ts
@@ -1,5 +1,4 @@
 import client from "./client";
-import { my_script } from "../timeline";
 
 export const load = () => {
   return client({
@@ -9,24 +8,22 @@ export const load = () => {
 };
 
 function convertScript(
-  data: Array<{ start: number; end: number; textEdited: string }>
+  data: Array<{ start: number; end: number; text: string }>
 ) {
-  return data.map(
-    (t: { start: number; end: number; textEdited: string }, idx) => {
-      return {
-        id: idx,
-        start:
-          String(Math.floor(t.start / 1000 / 60)).padStart(2, "0") +
-          ":" +
-          String(Math.floor(t.start / 1000) % 60).padStart(2, "0"),
-        end:
-          String(Math.floor(t.end / 1000 / 60)) +
-          ":" +
-          String(Math.floor(t.end / 1000) % 60).padStart(2, "0"),
-        text: t.textEdited,
-      };
-    }
-  );
+  return data.map((t: { start: number; end: number; text: string }, idx) => {
+    return {
+      id: idx,
+      start:
+        String(Math.floor(t.start / 60)).padStart(2, "0") +
+        ":" +
+        String(Math.floor(t.start) % 60).padStart(2, "0"),
+      end:
+        String(Math.floor(t.end / 60)) +
+        ":" +
+        String(Math.floor(t.end) % 60).padStart(2, "0"),
+      text: t.text,
+    };
+  });
 }
 export const upload = async (
   title: string,
@@ -61,12 +58,13 @@ export const upload = async (
           fileName: fileName,
         },
       });
-      const script = convertScript(my_script.segments);
+
+      const script = await (await fetch(data.textUrl)).json();
       const src = data.videoUrl.split("?")[0];
       return {
         status,
         url: src,
-        script,
+        script: convertScript(script),
         vtt: data.captionUrl,
       };
     } else {
@@ -84,7 +82,12 @@ export const upload = async (
       url: "https://s3-sowooju-video-an2.s3.ap-northeast-2.amazonaws.com/11-202203140939.mp4",
       vtt: "https://s3-sowooju-caption-an2.s3.ap-northeast-2.amazonaws.com/test.vtt",
     };
-    const script = convertScript(my_script.segments);
+
+    const script = await (
+      await fetch(
+        "https://s3-sowooju-text-an2.s3.ap-northeast-2.amazonaws.com/1-202205274931.json"
+      )
+    ).json();
     if (status === 200) {
       // 머신러닝 작동 호출
       const src = url.split("?")[0];
